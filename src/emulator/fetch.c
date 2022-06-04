e108eae4f94bc82d696f145f622b299ce9ef823a
Shiva Tamil Kumaran <anand.tk.03@gmail.com>
2022-06-04 13:51:13 +0100

    Finished fetch

--- src/emulator/fetch.c
+++ src/emulator/fetch.c
@@ -19,3 +19,23 @@ State *initARMState(const char *fileName) {
     return newState;
 }
 
+word getWord(State *armState, word address) {
+    if (address + 3 >= MEM_SIZE) {
+        perror("Tried to access memory out of bounds.");
+        exit(EXIT_FAILURE);
+    }
+
+    word fetched = 0;
+
+    for (int i = 0; i < WORD_IN_BYTES; i ++) {
+        fetched += armState->memory[address + i] << (i * BYTE);
+    }
+    
+    return fetched;
+}
+
+void fetch(State *armState) {
+    word pc = armState->registers[PC];
+    armState->fetched = getWord(armState, pc);
+    armState->registers[PC] += 4;
+}
--- src/emulator/fetch.h
+++ src/emulator/fetch.h
@@ -1,11 +1,20 @@
 #ifndef FETCH
 #define FETCH
 
+#include <stdio.h>
+#include <stdlib.h>
+
 #include "../definitions.h"
 #include "binFileLoader.h"
 
+#define BYTE 8
+
+// Gets a word from memory.
+word getWord(State *armState, word address);
+
 // Performs fetch part of cycle, and updates the cpu's state.
-// void fetch(State *cpuState);
+void fetch(State *armState);
+
 // Initialises a cpu state on the heap, with the instructions from a .bin file, 
 // and returns a pointer to this state struct.
 State *initARMState(const char *fileName);
