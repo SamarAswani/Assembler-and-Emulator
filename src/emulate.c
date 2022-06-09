7b61bb34770cc546c3070649a0676307ee04124b
Jai Hindocha <jai.hindocha@gmail.com>
2022-06-09 15:18:48 +0100

    testing works

--- src/emulate.c
+++ src/emulate.c
@@ -18,22 +18,23 @@ void printRegisters(State *state) {
             printf("$%4i: %10i (0x%08x)\n", i, state->registers[i], state->registers[i]);
         }    
     }
+    printf("Non-zero memory:\n");
+    for (int i = 0; i < MEM_SIZE; i += 4) {
+        word mem = getWord(state, i);
+        word littleEndian = (mem >> 24) | ((mem >> 8) & 0x0000ff00) | ((mem << 8) & 0x00ff0000) | (mem << 24);
+        if (littleEndian != 0) {
+            printf("0x%08x: 0x%08x\n", i, littleEndian);
+        }
+    }
 }
 
 int main(int argc, char **argv) {
     State *armState = initARMState("tst02");
-    // fetch(armState);
-    // printf("%x\n", armState->fetched);
-    // decode(armState);
-    while ((!armState->decoded.isSet || (armState->decoded.isSet && armState->decoded.instruction == 0)) 
-        && armState->registers[PC] < MEM_SIZE) {
+    while (!armState->decoded.isSet || (armState->decoded.isSet && armState->decoded.instruction)) {
         execute(armState);
         decode(armState);
         fetch(armState);
     }
-    // printf("%x\n", armState->decoded.i.dp.opcode);
-    // printf("%x\n", armState->decoded.isSet);
-    // printf("%x\n", armState->decoded.instruction);
     printRegisters(armState);
     destroyARMState(armState);
 }
